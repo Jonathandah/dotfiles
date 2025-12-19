7fbc04567bb13dd81e318bfcdf3261fe61e0e4bb
jonathan.md <github.com.starship007@passmail.net>
2025-12-19 22:24:41 +0100

    update nvim

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- <leader>r to replace word under cursor
+vim.keymap.set('n', '<leader>r', ':%s/\\V\\<<C-r><C-w>\\>//g<Left><Left>', { desc = 'Replace word under cursor' })

aab50847f793e227817a0e2c5b14f451cfcfeaf0
jonathan.md <github.com.starship007@passmail.net>
2026-01-02 18:52:56 +0100

    refactor mullvad script

---
+++ dot_config/waybar/scripts/executable_mullvad_vpn.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Mullvad CLI
+vpn_get_status() {
+  mullvad status
+}
+vpn_reconnect() {
+  mullvad reconnect
+}
+vpn_disconnect() {
+  mullvad disconnect
+}
+vpn_connect() {
+  mullvad connect
+}
+vpn_help() {
+  mullvad
+}
+
+# Utils
+build_tooltip() {
+  TOOLTIP="Mullvad VPN\n\nStatus: $VPN_CONNECTION\nRelay: $VPN_RELAY\nFeatures: $VPN_FEATURES\nVisible Location: $VPN_VISIBLE_LOCATION"
+}
+
+#Parsers
+parse_status() {
+  local status="$1"
+  VPN_CONNECTION=$(echo "$status" | head -n1 | xargs)
+  VPN_RELAY=$(echo "$status" | awk -F'Relay: ' '/Relay:/ {print $2}' | xargs)
+  VPN_FEATURES=$(echo "$status" | awk -F'Features: ' '/Features:/ {print $2}' | xargs)
+  VPN_VISIBLE_LOCATION=$(echo "$status" | awk -F'Visible location: ' '/Visible location:/ {print $2}' | xargs)
+}
+
+#Methods
+toggle_connection() {
+  if [ "$VPN_CONNECTION" = "Connected" ]; then
+    vpn_disconnect
+  else
+    vpn_connect
+  fi
+
+  notify-send
+  sleep 1
+}
+
+reconnect() {
+  vpn_reconnect
+
+  notify-send
+  sleep 1
+}
+
+get_status() {
+  local class alt
+  case "$VPN_CONNECTION" in
+  Connected)
+    class="connected"
+    alt="connected"
+    ;;
+  Connecting)
+    class="connecting"
+    alt="connecting"
+    ;;
+  *)
+    class="disconnected"
+    alt="disconnected"
+    ;;
+  esac
+
+  build_tooltip
+
+  echo "{\"text\": \"$VPN_CONNECTION\", \"tooltip\": \"$TOOLTIP\", \"alt\": \"$alt\", \"class\": \"$class\"}"
+}
+
+# Init
+parse_status "$(vpn_get_status)"
+
+#Args
+case "$1" in
+toggle) toggle_connection ;;
+reconnect) reconnect ;;
+status) get_status ;;
+*) vpn_help ;;
+esac

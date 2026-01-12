6177d59b73e7a3ded3bf0953bc601e6b9453b1fe
jonathan.md <github.com.starship007@passmail.net>
2026-01-12 16:26:11 +0100

    update lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -3,4 +3,23 @@
 -- Add any additional keymaps here
 
 -- <leader>r to replace word under cursor
-vim.keymap.set('n', '<leader>r', ':%s/\\V\\<<C-r><C-w>\\>//g<Left><Left>', { desc = 'Replace word under cursor' })
+vim.keymap.set("n", "<leader>r", ":%s/\\V\\<<C-r><C-w>\\>//g<Left><Left>", { desc = "Replace word under cursor" })
+
+-- <leader>dl to insert console.log for word under cursor
+vim.keymap.set("n", "<leader>dd", function()
+  local word = vim.fn.expand("<cword>")
+  local buf = 0
+  local row = vim.api.nvim_win_get_cursor(0)[1]
+  local last = vim.api.nvim_buf_line_count(buf)
+
+  while row <= last do
+    local line = vim.api.nvim_buf_get_lines(buf, row - 1, row, false)[1]
+
+    if line:match(";[%s]*$") or line:match("}%s*$") then
+      vim.api.nvim_buf_set_lines(buf, row, row, false, { 'console.log("' .. word .. ':", ' .. word .. ");" })
+      return
+    end
+
+    row = row + 1
+  end
+end, { desc = "Smart console.log placement" })
